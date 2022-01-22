dac120226c3f4891cf3ffd1c86050f11e5f7ac65
kirby_lam <kirbylam16@gmail.com>
2022-01-21 18:32:47 -0800

    Comment text area now clears when submitted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(comment_params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.author = current_user
     @comment.save
